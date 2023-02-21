28a6e9427c16dd7e9f0a3f1d9eb77f18cc3ba097
Johan <johan.9te@gmail.com>
2023-02-21 22:58:24 +0100

    missed main class

---
+++ main.py
@@ -0,0 +1,82 @@
+# Labbebot in python
+from pathlib import Path
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+PREFIX = os.getenv("PREFIX")
+DISCORD_STATUS = discord.Game(
+        "w̶̡͌i̷͉̚t̷̘̎h̶̙̀ ̸̙͊ḧ̶̯́i̴̳̾ṡ̷͚ ̷̾͜f̷͈͛r̸̬̾i̴̢̎e̷̠͒ñ̶̥d̵͜͝s̸̮̆"
+)
+
+# --------------------
+#
+#         Bot
+#
+# --------------------
+
+
+class LabbeBot(commands.Bot):
+    bot_app_info: discord.AppInfo
+
+    def __init__(self):
+        self._cogs = [p.stem for p in Path(".").glob("./cogs/*.py")]
+
+        intents = discord.Intents.default()
+        intents.members = True
+        intents.message_content = True
+
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(PREFIX),
+            case_insensitive=True, 
+            intents=intents
+        )
+
+    async def setup_hook(self):
+        self.bot_app_info = await self.application_info()
+
+    async def on_ready(self):
+        self.client_id = (await self.application_info()).id
+        
+        await self.change_presence(activity=DISCORD_STATUS)
+        await self.initialize_cogs()
+        await self.list_guilds()
+
+    async def initialize_cogs(self):
+        print("Loading cogs...")
+        for cog in self._cogs:
+            await self.load_extension(f"cogs.{cog}")
+            print(f"Loaded {cog} cog. ")
+
+    async def list_guilds(self):
+        print("\nYou are now live in the following guilds: \n")
+        async for guild in self.fetch_guilds(limit=5):
+            print(f'{guild.name}(id: {guild.id})')
+        print(" ")
+
+    async def on_connect(self):
+        print(f"Connected to Discord (latency: {self.latency*1000:,.0f} ms). ")
+
+    async def close(self):
+        print("Closing on keyboard interrupt...")
+        print("Shutting down bot... ")
+        await super().close()
+
+    async def on_disconnect(self):
+        print("Bot disconnected. ")
+
+    async def on_resumed(self):
+        print("Bot resumed. ")
+
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.process_commands(message)
+
+bot = LabbeBot()
+
+if __name__ == "__main__":
+    bot.run(DISCORD_TOKEN)
